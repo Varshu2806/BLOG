df98adf6b5d90553248f40239dc18d26065c7590
Varshu2806 <varshiniavvari09@gmail.com>
2025-06-20 16:22:59 +0530

    Update models and admin setup

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Summary(models.Model):
+    title=models.CharField(max_lenghth=250)
+    content=models.TextField()
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
